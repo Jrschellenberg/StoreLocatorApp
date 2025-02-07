80250c71114092b94ee7f4f387f044ddd38963dc
Justin Schellenberg <jrschellenberg@gmail.com>
2025-02-07 09:52:12 -0600

    update with data and example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
 
-const App = () => (
+const App = ({filteredLocations}) => (
   <div>
 
       <h1>Hello world</h1>
 
+    {filteredLocations.map(m => <h2 key={m.name}>{m.name}</h2>)}
   </div>
 );
 export default App;
--- src/fnd.entry.app.jsx
+++ src/fnd.entry.app.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const FND = (window.FND = window.FND || {});
+FND.BUILDS = FND.BUILDS || {};
+FND.BUILDS.InitializeStoreLocator = (props) => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+      <App {...props} />
+    </React.StrictMode>
+  );
+}
